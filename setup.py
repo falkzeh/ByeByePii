301cccc1b0d783e62119a0d11ce0aca1095de994
falk.zeh <falk.zeh@tonies.com>
2022-06-14 10:52:46 +0200

    rename readme

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "readme.md").read_text()
+long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="ByeByePii",
-    version="0.0.2",
+    version="0.0.3",
     author="Falk Z.",
     description="A package for hashing personal identifiable information (PII).",
     long_description=long_description,
